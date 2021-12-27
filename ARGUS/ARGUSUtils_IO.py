78450d0cf6f15d02efacabcd301aadb551b9eac7
forrest <forrest@sb3-kitware.local>
2021-12-27 14:09:01 -0500

    ARGUS: close av container

--- ARGUS/ARGUSUtils_IO.py
+++ ARGUS/ARGUSUtils_IO.py
@@ -13,13 +13,18 @@ def load_video(filename,height=0,width=0):
     if(height==0 or width==0):
         height,width = shape_video(filename)
 
-    container = av.open(filename)
-    stream = container.streams.video[0]
-    stream.thread_type = 'AUTO'
-    
-    num_frames = stream.frames
-    
-    frames = np.empty((num_frames,height,width))
-    for i,frame in enumerate(container.decode(stream)):
-        frames[i] = frame.to_ndarray(format='gray')
-    return frames
+    container = None
+    try:
+        container = av.open(filename)
+        stream = container.streams.video[0]
+        stream.thread_type = 'AUTO'
+        
+        num_frames = stream.frames
+        
+        frames = np.empty((num_frames,height,width))
+        for i,frame in enumerate(container.decode(stream)):
+            frames[i] = frame.to_ndarray(format='gray')
+        return frames
+    finally:
+        if container:
+            container.close()
