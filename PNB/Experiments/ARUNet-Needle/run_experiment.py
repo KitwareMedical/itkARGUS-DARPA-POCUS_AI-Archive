c5c47ef9d2aaf63350e1ac5bb6dc0bfc1a50f70e
Stephen Aylward <stephen.aylward@kitware.com>
2022-08-15 09:20:08 -0400

    ENH: Using pre-processed data for needle-only seg task

--- PNB/Experiments/ARUNet-Needle/run_experiment.py
+++ PNB/Experiments/ARUNet-Needle/run_experiment.py
@@ -2,14 +2,14 @@ from torch.multiprocessing import freeze_support
 from ARGUS_Needle_Experiment import train_parallel_runs
 
 def run_experiment():
-    img_dir = "../../Data_PNB/Preprocessed"
-    anno_dir = "../../Data_PNB/annotations/Sean_May22_CLEANED"
+    img_dir = "../../Data_PNB/stephen_prep"
+    anno_dir = "../../Data_PNB/stephen_prep"
 
-    num_runs = 5
-    vfolds = [0, 1, 2, 3, 4]
-    devices = [0, 1, 2]
+    runs = [0,1,2]
+    vfolds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    devices = [0,1]
 
-    train_parallel_runs(img_dir, anno_dir, num_runs, vfolds, devices)
+    train_parallel_runs(img_dir, anno_dir, runs, vfolds, devices)
 
 if __name__ == '__main__':
     freeze_support()
