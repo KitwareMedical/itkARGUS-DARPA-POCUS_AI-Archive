54ee0652875c55d8a321b9ec854fdd34549e47bb
Stephen Aylward <stephen.aylward@kitware.com>
2022-12-14 12:55:51 -0500

    ENH: Reorganize PreTrain directory

--- PreTrain/run0.py
+++ PreTrain/run0.py
@@ -6,6 +6,6 @@ nnet.setup_vfold_files()
 nnet.setup_training_vfold(0)
 
 nnet.max_epochs = 5000
-run_num = 1
+run_num = 0
 device_num = 0
 nnet.train_vfold(run_num,device_num)
--- PreTrain/run2.py
+++ PreTrain/run2.py
@@ -6,6 +6,6 @@ nnet.setup_vfold_files()
 nnet.setup_training_vfold(0)
 
 nnet.max_epochs = 5000
-run_num = 0
-device_num = 3
+run_num = 2
+device_num = 2
 nnet.train_vfold(run_num,device_num)
