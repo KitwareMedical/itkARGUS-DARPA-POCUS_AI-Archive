b53e8d075d088d852e7f419f2527fb94b78e80be
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-12-16 17:22:51 -0500

    ENH: Initial pre-training completed (#105)
    Includes randomized train, validation, test folds.

---
+++ PreTrain/run1.py
@@ -0,0 +1,11 @@
+from ARGUS_Network import ARGUS_Network
+
+nnet = ARGUS_Network("PreTrainPTX")
+nnet.setup_vfold_files()
+
+nnet.setup_training_vfold(0)
+
+nnet.max_epochs = 5000
+run_num = 1
+device_num = 1
+nnet.train_vfold(run_num,device_num)
