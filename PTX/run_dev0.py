08a41d6fd05bc32c3c98a33b1ab6dbc8e04d60de
Stephen Aylward <stephen.aylward@kitware.com>
2022-12-29 16:42:32 -0500

    ENH: Starting to update pretrain to use scripts

--- PTX/run_dev0.py
+++ PTX/run_dev0.py
@@ -1,10 +1,13 @@
-from ARGUS_train_ptx_ar import ARGUS_train_ptx_ar
+import site
+site.addsitedir("../ARGUS")
 
-nnet = ARGUS_train_ptx_ar("vfold", device_num=0)
+from ARGUS_ptx_ar_train import ARGUS_ptx_ar_train
 
-nnet.setup_vfold_files()
-
-for run_num in range(3):
+for run_num in range(1):
+    nnet = ARGUS_ptx_ar_train("../ARGUS/ARGUS_ptx_ar.cfg", "vfold", device_num=0)
+    nnet.setup_vfold_files()
     for vfn in range(nnet.num_folds):
         nnet.setup_training_vfold(vfn)
+        pretrained_model_name = "./Pretrained_Models/pretrain_ptx_final_run"+str(run_num)+"/best_model_0.pth"
+        nnet.load_model(run_num, pretrained_model_name)
         nnet.train_vfold(run_num)
