bccff1903a9bd8f093e563585c39baa76db63e04
Stephen Aylward <stephen.aylward@kitware.com>
2023-01-01 15:33:05 -0500

    ENH: Pretrain using 50% of data and standard classes

--- Pretrain/Data_Pretrain/Data_Raw/README.md
+++
@@ -1,1 +0,0 @@
-The full collection of data from the various tasks is aggregated here.
---
+++ Pretrain/run_half1.py
@@ -0,0 +1,12 @@
+import site
+site.addsitedir("../ARGUS")
+
+from ARGUS_segmentation_train import ARGUS_segmentation_train
+
+nnet = ARGUS_segmentation_train("ARGUS_pretrain_half_ar.cfg", "vfold", device_num=0)
+nnet.setup_vfold_files()
+
+run_num = 1
+for vfn in range(nnet.num_folds):
+    nnet.setup_training_vfold(vfn)
+    nnet.train_vfold(run_num)
