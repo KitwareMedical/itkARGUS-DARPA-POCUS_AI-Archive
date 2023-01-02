c78aabcfd4acdc673fa6c04509d1a6142b38f981
Stephen Aylward <stephen.aylward@kitware.com>
2023-01-02 10:15:26 -0500

    BUG: Fix GPU instance for pretraining runnings

--- Pretrain/run_half1.py
+++ Pretrain/run_half1.py
@@ -3,10 +3,10 @@ site.addsitedir("../ARGUS")
 
 from ARGUS_segmentation_train import ARGUS_segmentation_train
 
-nnet = ARGUS_segmentation_train("ARGUS_pretrain_half_ar.cfg", "vfold", device_num=0)
+nnet = ARGUS_segmentation_train("ARGUS_pretrain_half_ar.cfg", "vfold", device_num=1)
 nnet.setup_vfold_files()
 
 run_num = 1
-for vfn in range(nnet.num_folds):
+for vfn in range(3,nnet.num_folds):
     nnet.setup_training_vfold(vfn)
     nnet.train_vfold(run_num)
